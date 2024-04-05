9da3e59481cc8ff3f8bc8863ac9b63b38eec77d0
VeronikaTsitser <kondratukveronika@gmail.com>
2024-04-05 15:06:08 +0100

    рисуем прямые линии

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,11 +21,89 @@ class ShapesDrawingApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GlobalKey customPaintKey = GlobalKey();
+  List<Offset?> startPoints = [];
+  List<Offset?> endPoints = [];
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.yellow,
+      body: GestureDetector(
+        onPanStart: (details) {
+          RenderBox renderBox = context.findRenderObject() as RenderBox;
+          Offset localPosition = renderBox.globalToLocal(details.globalPosition);
+          setState(() {
+            startPoints.add(localPosition);
+            endPoints.add(localPosition); // Добавляем ту же точку в качестве начала
+          });
+        },
+        onPanUpdate: (details) {
+          RenderBox renderBox = context.findRenderObject() as RenderBox;
+          Offset localPosition = renderBox.globalToLocal(details.globalPosition);
+          setState(() {
+            endPoints[endPoints.length - 1] = localPosition; // Обновляем последнюю точку
+          });
+        },
+        onPanEnd: (details) {
+          setState(() {
+            // Не добавляем ничего, оставляем точки как есть
+          });
+        },
+        child: CustomPaint(
+          key: customPaintKey,
+          painter: ShapesPainter(startPoints, endPoints),
+          child: Container(),
+        ),
+      ),
+    );
   }
+}
+
+class ShapesPainter extends CustomPainter {
+  ShapesPainter(this.startPoints, this.endPoints);
+  final List<Offset?> startPoints;
+  final List<Offset?> endPoints;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint linePaint = Paint()
+      ..color = Colors.black
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
+
+    Paint pointPaint = Paint()
+      ..color = Colors.blue
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 10.0;
+
+    Paint bigPointPaint = Paint()
+      ..color = Colors.white
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 15.0;
+
+    for (int i = 0; i < startPoints.length; i++) {
+      if (startPoints[i] != null && endPoints[i] != null) {
+        // Рисуем линию
+        canvas.drawLine(startPoints[i]!, endPoints[i]!, linePaint);
+
+        // Рисуем начальную и конечную точку
+        canvas.drawCircle(startPoints[i]!, 7.0, bigPointPaint);
+        canvas.drawCircle(startPoints[i]!, 5.0, pointPaint);
+        canvas.drawCircle(endPoints[i]!, 7.0, bigPointPaint);
+        canvas.drawCircle(endPoints[i]!, 5.0, pointPaint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
